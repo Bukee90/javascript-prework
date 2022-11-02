185c759c9bbd39e0f1255fe5986e6f74dc5aa416
Dawid Bogusz <dawid.bogusz90.kursy@gmail.com>
2022-11-02 15:06:00 +0100

    add 'game rules' to script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,47 @@
-let computerMove = `kamień`;
-let playerMove = 'papier'
+//Player
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to'  + playerMove + ', to wygrywasz!');
+console.log('Gracz wybrał: ' + playerInput);
+
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier'
+} else {
+    playerMove = 'nożyce'
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+
+//PC
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+if(randomNumber == 1){
+    pcMove = 'kamień';
+} else if (randomNumber == 2) {
+    pcMove = 'papier'
+} else {
+    pcMove = 'nożyce'
+}
+
+console.log('Computer wybrał ' + randomNumber);
+printMessage('Pc ruch to: ' + pcMove);
+
+//Result
+if (playerInput == randomNumber) {
+    printMessage ('Remis')
+} else if (playerInput == '1' && randomNumber == '2') {
+    printMessage ('Przegrałeś')
+} else if (playerInput == '1' && randomNumber == '3') {
+    printMessage ('Wygrałeś')
+} else if (playerInput == '2' && randomNumber == '1') {
+    printMessage ('Wygrałeś')
+} else if (playerInput == '2' && randomNumber == '3') {
+    printMessage ('Przegrałeś')
+} else if (playerInput == '3' && randomNumber == '1') {
+    printMessage ('Przegrałeś')
+} else if (playerInput == '3' && randomNumber == '2') {
+    printMessage ('Wygrałeś')
+}
